3b52f1403f52fefc2717d2abedc3d2ca05652e74
ratul2f8 <ratuldasanik@outlook.com>
2021-04-08 13:24:31 +0600

    v2-applying security with permission using annotations instead of antMatchers

--- src/main/java/com/example/securityTut/student/StudentManagementController.java
+++ src/main/java/com/example/securityTut/student/StudentManagementController.java
@@ -1,5 +1,6 @@
 package com.example.securityTut.student;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
@@ -14,18 +15,22 @@ public class StudentManagementController {
             new Student("John", 3)
     );
     @GetMapping
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN','ROLE_ADMINTRAINEE')")
     public List<Student> getAllStudents(){
         return STUDENTS;
     }
     @PostMapping
+    @PreAuthorize("hasAuthority('student:write')")
     public void registerNewStudent(@RequestBody Student student){
         System.out.println(student);
     }
     @DeleteMapping(path = "{studentId}")
+    @PreAuthorize("hasAuthority('student:write')")
     public void deleteStudent(@PathVariable("studentId") Integer id){
         System.out.println(id);
     }
     @PutMapping(path = "{studentId}")
+    @PreAuthorize("hasAuthority('student:write')")
     public void updateStudent(@PathVariable("studentId") Integer studentId, @RequestBody Student student){
         System.out.printf("%s : %s%n", studentId, student);
     }
